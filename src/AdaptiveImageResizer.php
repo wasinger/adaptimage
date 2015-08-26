a6809db4330dc07f49d37e4a21e5901a4237fa01
Christoph Singer <singer@webagentur72.de>
2015-08-26 17:15:36 +0200

    factory function for AdaptiveImageResizer

--- src/AdaptiveImageResizer.php
+++ src/AdaptiveImageResizer.php
@@ -2,6 +2,7 @@
 namespace Wa72\AdaptImage;
 
 use Imagine\Image\ImagineInterface;
+use Wa72\AdaptImage\Output\OutputPathNamerBasedir;
 use Wa72\AdaptImage\Output\OutputPathNamerInterface;
 
 /**
@@ -112,4 +113,27 @@ class AdaptiveImageResizer
         return $resizer->resize($image, $really_do_it);
     }
 
+    /**
+     * Factory function for creating an AdaptiveImageResizer
+     * for an array of allowed image widths (and unlimited height)
+     *
+     * @param int[] $widths Array of allowed image width values in pixels
+     * @param ImagineInterface $imagine    The Imagine instance for scaling the images.
+     * @param OutputPathNamerInterface|null $output_path_namer  The OutputPathNamer for generating the names
+     *                                                          of the cached files. If none is provided, a new
+     *                                                          instance of OutputPathNamerBasedir will be created
+     *                                                          with directory sys_get_temp_dir() . '/ai_cache'
+     * @return AdaptiveImageResizer
+     */
+    static public function create($widths, $imagine, $output_path_namer = null)
+    {
+        if (!($output_path_namer instanceof OutputPathNamerInterface)) {
+            $output_path_namer = new OutputPathNamerBasedir(sys_get_temp_dir() . '/ai_cache');
+        }
+        $irds = array();
+        foreach ($widths as $width) {
+            $irds[] = ImageResizeDefinition::create($width, INF);
+        }
+        return new self($imagine, $output_path_namer, $irds);
+    }
 }
