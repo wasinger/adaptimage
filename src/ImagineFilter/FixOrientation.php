5501da3904589e5dfef8e60f46a2c1ea5b94ce35
Christoph Singer <singer@webagentur72.de>
2017-08-11 22:45:07 +0200

    FixOrientation: add missing orientation cases

--- src/ImagineFilter/FixOrientation.php
+++ src/ImagineFilter/FixOrientation.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: christoph
- * Date: 09.02.15
- * Time: 12:34
- */
-
 namespace Wa72\AdaptImage\ImagineFilter;
-
 
 use Imagine\Filter\FilterInterface;
 use Imagine\Image\Box;
@@ -33,12 +25,34 @@ class FixOrientation implements  FilterInterface, ResizingFilterInterface
      */
     public function apply(ImageInterface $image)
     {
+
+        //        1        2       3      4         5            6           7          8
+        //
+        //        888888  888888      88  88      8888888888  88                  88  8888888888
+        //        88          88      88  88      88  88      88  88          88  88      88  88
+        //        8888      8888    8888  8888    88          8888888888  8888888888          88
+        //        88          88      88  88
+        //        88          88  888888  888888
+
+
         switch ($this->orientation) {
+            case 2:
+                $image->flipHorizontally();
+                break;
             case 3:
                 $image->rotate(180);
                 break;
+            case 4:
+                $image->flipVertically();
+                break;
+            case 5:
+                $image->rotate(-90)->flipHorizontally();
+                break;
             case 6:
                 $image->rotate(90);
+                break;
+            case 7:
+                $image->rotate(90)->flipHorizontally();
                 break;
             case 8:
                 $image->rotate(-90);
@@ -46,9 +60,10 @@ class FixOrientation implements  FilterInterface, ResizingFilterInterface
             default:
                 break;
         }
-        // reset orientation in Exif data to 0
+
+        // after rotating the image, we need to reset orientation in Exif data to 1
         if ($image instanceof \Imagine\Imagick\Image) {
-            $image->getImagick()->setImageOrientation(0);
+            $image->getImagick()->setImageOrientation(1);
         } else {
             // TODO: how can this be done in GD and Gmagick?
         }
@@ -65,7 +80,8 @@ class FixOrientation implements  FilterInterface, ResizingFilterInterface
      */
     public function calculateSize(BoxInterface $size)
     {
-        if ($this->orientation == 6 || $this->orientation == 8) {
+        if ($this->orientation >= 5) {
+            // in orientation values >= 5 width and height are swapped
             return new Box($size->getHeight(), $size->getWidth());
         } else {
             return $size;
