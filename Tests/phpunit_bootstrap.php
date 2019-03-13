bb43f7de4b246a7a1413939dd32ede5b971fae8c
Christoph Singer <christoph.singer@tuebingen.de>
2019-03-13 17:42:01 +0100

    PHPunit configuration: use local vendor dir first

--- Tests/phpunit_bootstrap.php
+++ Tests/phpunit_bootstrap.php
@@ -1,15 +1,13 @@
 <?php
-// if we are checked out as a stand-alone project
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    // if we are checked out as a stand-alone project
     $loader = __DIR__ . '/../vendor/autoload.php';
-}
- 
-// if we are within the vendor directory of another project
-if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+} else if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    // if we are within the vendor directory of another project
     $loader = __DIR__ . '/../../../../vendor/autoload.php';
 }
  
-if (!$loader = @include($loader)) {
+if (!isset($loader) || !$loader = @include($loader)) {
     echo <<<EOM
 You must set up the project dependencies by running the following commands:
  
